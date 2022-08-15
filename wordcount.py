0b7b13a63e80344dfddcf48275fe0e731d69da6e
Michaela Healton <mshealton@gmail.com>
2022-08-15 12:00:22 -0700

    Word Count Version 1

--- wordcount.py
+++ wordcount.py
@@ -1,4 +1,25 @@
-"""Count words in file."""
+"""Count words in file.
+As I was going to St. Ives
+I met a man with seven wives
+Every wife had seven sacks
+Every sack had seven cats
+Every cat had seven kits
+Kits, cats, sacks, wives.
+How many were going to St. Ives?
 
+"""
 
 # put your code here.
+def le_words(file_name):
+  file = open(file_name)
+
+  count_words = {}
+
+  for line in file:
+    line = line.rstrip()
+    line = line.split(" ")
+    for word in line:
+        count_words[word] = count_words.get(word, 0) + 1 
+  print(count_words)
+le_words("test.txt")
+le_words("twain.txt")
